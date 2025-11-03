66ccedb3871c4158f9b6e4efe8081b5dc99ecea6
Chetan6010 <ankireddychetan2004@gmail.com>
2025-11-03 15:31:12 +0530

    Updated config and App.js

--- hms-frontend/src/App.js
+++ hms-frontend/src/App.js
@@ -62,7 +62,7 @@ function App() {
         />
         
         <Route
-          path="/dashboard/doctor"
+          path="/dashboard/DoctorDashboard"
           element={
             <ProtectedRoute role="doctor">
               <DoctorDashboard />
