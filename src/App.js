c8c1aa2f5b1bf72a8b3a41aca1480d032e63fc3d
PoluriLakshmiSatwika <lakshmisatwika29@gmail.com>
2025-11-11 13:03:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
@@ -80,7 +58,7 @@ function App() {
 
       <Footer />
     </>
->>>>>>> d21006d (Initial commit for frontend)
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -31,4 +13,3 @@ root.render(
     </AuthProvider>
   </BrowserRouter>
 );
->>>>>>> d21006d (Initial commit for frontend)
