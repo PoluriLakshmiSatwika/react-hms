210547756b8d24964d6c6391e3fc5fdb99e6fd16
PoluriLakshmiSatwika <lakshmisatwika29@gmail.com>
2025-11-11 13:54:10 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import RoleSelectionPage from "./pages/RoleSelectionPage";
