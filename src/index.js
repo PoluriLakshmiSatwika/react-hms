006b3b32aeeff0e71c514d5aaf96f13522c4c153
PoluriLakshmiSatwika <lakshmisatwika29@gmail.com>
2025-11-12 12:03:00 +0530

    hashrouter added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/react-hms">
+  <HashRouter basename="/react-hms">
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
