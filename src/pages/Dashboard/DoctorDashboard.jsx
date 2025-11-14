e558d4a8d896049bc5c447bf2f6cee879e1c98e6
PoluriLakshmiSatwika <lakshmisatwika29@gmail.com>
2025-11-14 15:34:27 +0530

    doctors by specialty added

--- src/pages/Dashboard/DoctorDashboard.jsx
+++ src/pages/Dashboard/DoctorDashboard.jsx
@@ -88,7 +88,7 @@ const DoctorDashboard = () => {
   const fetchNurses = async () => {
     try {
       const res = await fetch(
-        `${process.env.REACT_APP_API_URL}/api/doctor/nurses`
+        `${process.env.REACT_APP_API_URL}/api/admin/nurses`
       );
       const data = await res.json();
       if (data.success) setNurses(data.data);
