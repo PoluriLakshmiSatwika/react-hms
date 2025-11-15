dac643113032d9978580c3a46a787cd23261324e
PoluriLakshmiSatwika <lakshmisatwika29@gmail.com>
2025-11-15 13:36:08 +0530

    nurse_login_updated

--- src/pages/Login/NurseLogin.jsx
+++ src/pages/Login/NurseLogin.jsx
@@ -50,9 +50,8 @@ const NurseLoginPage = () => {
     setErrors({});
 
     try {
-      const res = await fetch(
-        "https://hms-backend-op3l.onrender.com/api/nurse/login",
-        {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/nurse/login`,{
+        
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
